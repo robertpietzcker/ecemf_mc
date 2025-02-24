2fee2dc1d9a0203afc837b1e47fa56d689c44c48
robertpietzcker <pietzcker@pik-potsdam.de>
2025-02-24 15:23:31 +0100

    add new plots, update download

--- download_iiasa_db.py
+++ download_iiasa_db.py
@@ -8,7 +8,7 @@ def download_iiasa_meta_py(fileName, db, default_only = False):
 
     conn = pyam.iiasa.Connection(db)
     
-    df = conn.properties(default=default_only)
+    df = conn.properties(default_only=default_only)
     
     df.to_excel(fileName + ".xlsx")
 
